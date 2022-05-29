c6327db90772e0c353e64e0da83061d6a45dbc4c
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-29 22:10:49 +0000

    tarea lista

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -6,7 +6,11 @@ export const Cards = () => {
 			<div className="row row-cols-1 row-cols-md-4 g-4">
 				<div className="col">
 					<div className="card">
-						<img src="..." className="card-img-top" alt="..." />
+						<img
+							src="https://via.placeholder.com/200x100"
+							className="card-img-top"
+							alt="..."
+						/>
 						<div className="card-body">
 							<h5 className="card-title">Card title</h5>
 							<p className="card-text">
@@ -14,12 +18,19 @@ export const Cards = () => {
 								as a natural lead-in to additional content. This
 								content is a little bit longer.
 							</p>
+							<a href="#" class="btn btn-primary">
+								Go somewhere
+							</a>
 						</div>
 					</div>
 				</div>
 				<div className="col">
 					<div className="card">
-						<img src="..." className="card-img-top" alt="..." />
+						<img
+							src="https://via.placeholder.com/200x100"
+							className="card-img-top"
+							alt="..."
+						/>
 						<div className="card-body">
 							<h5 className="card-title">Card title</h5>
 							<p className="card-text">
@@ -27,24 +38,38 @@ export const Cards = () => {
 								as a natural lead-in to additional content. This
 								content is a little bit longer.
 							</p>
+							<a href="#" class="btn btn-primary">
+								Go somewhere
+							</a>
 						</div>
 					</div>
 				</div>
 				<div className="col">
 					<div className="card">
-						<img src="..." className="card-img-top" alt="..." />
+						<img
+							src="https://via.placeholder.com/200x100"
+							className="card-img-top"
+							alt="..."
+						/>
 						<div className="card-body">
 							<h5 className="card-title">Card title</h5>
 							<p className="card-text">
 								This is a longer card with supporting text below
 								as a natural lead-in to additional content.
 							</p>
+							<a href="#" class="btn btn-primary">
+								Go somewhere
+							</a>
 						</div>
 					</div>
 				</div>
 				<div className="col">
 					<div className="card">
-						<img src="..." className="card-img-top" alt="..." />
+						<img
+							src="https://via.placeholder.com/200x100"
+							className="card-img-top"
+							alt="..."
+						/>
 						<div className="card-body">
 							<h5 className="card-title">Card title</h5>
 							<p classNames="card-text">
@@ -52,6 +77,9 @@ export const Cards = () => {
 								as a natural lead-in to additional content. This
 								content is a little bit longer.
 							</p>
+							<a href="#" class="btn btn-primary">
+								Go somewhere
+							</a>
 						</div>
 					</div>
 				</div>
